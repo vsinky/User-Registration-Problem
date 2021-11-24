9b219c08580dfd69188eec43c35cebed94218cc5
vsinky <sinkykumariverma@gmail.com>
2021-11-24 22:43:09 +0530

    Added: last name

--- src/com/bridgelabz/regex/Pattern.java
+++ src/com/bridgelabz/regex/Pattern.java
@@ -3,7 +3,7 @@ package com.bridgelabz.regex;
 import java.util.Scanner;
 
 public class Pattern {
-	private String firstName;
+	private String firstName , lastName;
 
 	Scanner sc = new Scanner(System.in);
 
@@ -19,12 +19,37 @@ public class Pattern {
 			isValidFirstName();
 		}
 	}
+	
+	public void isValidLastName() {
 
+		System.out.print("Enter Last Name : ");
+		lastName = sc.nextLine();
+		boolean check = lastName.matches("^([A-Z]{1}+[a-z]{2,})*$");
+		if (check == true) {
+			System.out.println("Valid");
+		} else {
+			System.out.println("Invalid ");
+			isValidLastName();
+		}
+	}
 	public String getFirstName() {
 		return firstName;
 	}
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void print() {
+	System.out.println("[firstName=" + firstName + ", lastName=" + lastName + "]");
+		
 	}
 }
---
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.regex;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		Pattern pattern = new Pattern();
+
+		pattern.isValidFirstName();
+		pattern.isValidLastName();
+        pattern.print();
+        
+	}
+}
